63c44b3bf4385d9a5a3f0fd3b8090dda23ecc9e2
github username <github email address>
2021-05-06 22:59:22 -0400

    commit tree d38037b47bf92d97b08791eac00c2134f992d0c0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,49 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    #why cant i go to /recipes/new ?
+    #is it bc the form is sending it to update the recipes page itself
+    #the new page is just the form, it doesnt have the information.
+    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    #this route sends the edited information from get
+    #the get page has a form that routes itself to this page
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+ delete '/recipes/:id' do
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.delete
+  redirect to "/recipes"
+ end
+
 
 end
